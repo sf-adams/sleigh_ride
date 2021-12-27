b55140103a0218776a038402a4746b326b784381
Sam <samuel.fitz@btinternet.com>
2021-12-27 17:08:10 +0000

    Updated Countdown For 2022

--- client/src/components/Countdown.js
+++ client/src/components/Countdown.js
@@ -10,7 +10,7 @@ const Countdown = () => {
     let interval = useRef();
 
     const startTimer = () => {
-        const countdownDate  = new Date('January 04 2022 00:00:00').getTime();
+        const countdownDate  = new Date('December 25 2022 00:00:00').getTime();
 
         interval = setInterval(() => {
             const now = new Date().getTime();
